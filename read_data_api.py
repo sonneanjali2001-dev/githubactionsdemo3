1f613144b2f79557589248cbb7fb9079453367eb
sonneanjali2001-dev <anjalisonni65@gmail.com>
2025-11-11 15:12:42 +0530

    code1

--- read_data_api.py
+++ read_data_api.py
@@ -1,9 +1,13 @@
 import requests
 import pandas as pd
+import os
 
-response=requests.get("https://jsonplaceholder.typicode.com/users")
-data = response.json()
-df = pd.DataFrame(data)
-answer=df[["id","name"]]
+token = os.getenv("API_TOKEN")
+print(f"Token : {token}")
 
-print(answer)
+#response=requests.get("https://jsonplaceholder.typicode.com/users")
+#data = response.json()
+#df = pd.DataFrame(data)
+#answer=df[["id","name"]]
+
+#print(answer)
